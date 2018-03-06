707406e0f76cb57d7553b04800916842776c98ea
Jesper Rønn-Jensen <jesperrr@gmail.com>
2018-03-06 11:34:02 +0100

    more history to remember

--- source/50_history.sh
+++ source/50_history.sh
@@ -7,12 +7,12 @@ shopt -s histverify
 
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
-export HISTCONTROL="ignorespace:erasedups"
+#export HISTCONTROL="ignorespace:erasedups"
 # Give history timestamps.
 export HISTTIMEFORMAT="[%F %T] "
 # Lots o' history.
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTSIZE=15000
+export HISTFILESIZE=30000
 
 # Easily re-execute the last history command.
 alias r="fc -s"
