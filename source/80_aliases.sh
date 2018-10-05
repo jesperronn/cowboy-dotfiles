6bdbf656d9e8497937d80caceb27c0b657dad8be
Jesper Rønn-Jensen <EMAIL>
2018-10-05 09:13:13 +0200

    git completion via homebrew

--- source/80_aliases.sh
+++ source/80_aliases.sh
@@ -27,11 +27,3 @@ alias chromium="/Applications/Chromium.app/Contents/MacOS/Chromium"
 # ssh completion
 # see http://hints.macworld.com/article.php?story=20100113142633883
 complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh
-
-
-
-DST=~/bin/.git-completion.bash
-if [ ! -f $DST ]; then
-  "$DOTFILES/bin/setup/git-completion.sh"
-fi
-. $DST
