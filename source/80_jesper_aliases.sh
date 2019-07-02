25fde1778c0e83322f48fc93142576334d8c2e74
Jesper Rønn-Jensen <jesperrr@gmail.com>
2019-07-02 09:38:23 +0200

    new LC_ALL settings

--- source/80_jesper_aliases.sh
+++ source/80_jesper_aliases.sh
@@ -17,6 +17,7 @@ export BUNDLER_EDITOR=atom
 # lang settings, view with `locale` or `locale -a`
 # export LANG=da_DK.UTF-8
 export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 # oracle instantclient via homebrew
 #
