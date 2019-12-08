0bb9796e972bb394d1d48c99bac94d203a4ced9e
Jesper Rønn-Jensen <jesperrr@gmail.com>
2019-12-08 21:35:04 +0100

    xpath/xml linter shortcuts with catalogs

--- source/70_karnov.sh
+++ source/70_karnov.sh
@@ -12,3 +12,16 @@ alias j='jinborov'
 
 alias cdk='cd ~/src/karnov'
 alias cda='cd ~/src/karnov/amp'
+
+# aliases for quick lint/transform via xml lib standard methods
+export KAR_CATALOG_PRJ=/home/marc/karnov/git/ns-karnovgroup-com
+export XML_CATALOG_PRJ=~/src/karnov/ns-karnovgroup-com
+alias karnov-catalog-ilse='XML_CATALOG_FILES=  xmlcatalog $XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-ilse-variant-utf-8.xml'
+alias karnov-catalog-strict='XML_CATALOG_FILES=  xmlcatalog $XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-utf-8.xml'
+alias karnov-doctype='tmp_func(){ XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-entities-only-utf-8.xml xmllint --dtdattr --noent --nonet --encode UTF-8 --format "$@" | grep -m 1 "<!DOCTYPE ";  unset -f tmp_func; }; tmp_func'
+alias karnov-dtd-ilse='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-ilse-variant-utf-8.xml xmllint --valid --noent --nonet --noout'
+alias karnov-dtd-strict='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-utf-8.xml xmllint --valid --noent --nonet --noout'
+alias karnov-root='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-entities-only-utf-8.xml xmllint --dtdattr --noent --nonet --xpath '\''name(/*)'\'''
+alias karnov-utf-8='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-entities-only-utf-8.xml xmllint --dtdattr --noent --nonet --encode UTF-8'
+alias karnov-xpath='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-entities-only-utf-8.xml xmllint --dtdattr --noent --nonet --xpath'
+alias karnov-xslt1='XML_CATALOG_FILES=$XML_CATALOG_PRJ/ns.karnovgroup.com/catalog-ilse-variant-utf-8.xml xsltproc --nonet'
