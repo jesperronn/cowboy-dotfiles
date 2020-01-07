4146da6f17b74d7ac6d1d624ed72e31ca9f6d96c
Jesper Rønn-Jensen <jesperrr@gmail.com>
2020-01-07 01:28:25 +0100

    prompt: timestamp right aligned. Removing user@hostname from prompt

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -124,6 +124,16 @@ function __prompt_command() {
   # __prompt_hg || \
   # svn: [repo:lastchanged]
   # __prompt_svn
+  # date: [HH:MM:SS]
+  # PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+  # date printed right side:
+  # PS1="$PS1\[$(tput sc; __prompt_timestamp; tput rc)\]"
+  # date: [HH:MM:SS] printed with \t
+
+  compensate=6 #compensate width for color codes used (in '$c1[\t]')
+  right_col=$(($(tput cols)+$compensate))
+  PS1="$PS1$( printf "%*s\r$sc9" "$right_col" "$c1[\t]" )"
+
   # Iterate over all vcs info parts, outputting an escaped var name that will
   # be interpolated automatically. This ensures that malicious branch names
   # can't execute arbitrary commands. For more info, see this PR:
@@ -140,14 +150,12 @@ function __prompt_command() {
   fi
   # misc: [cmd#:hist#]
   # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
-  # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
-  PS1="$PS1\n"
-  # date: [HH:MM:SS]
-  PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+
+  # print the [path]
+  PS1="$PS1$c1[$c0\w$c1]$c9"
   # exit code: 127
   PS1="$PS1$(__prompt_exit_code "$exit_code")"
-  PS1="$PS1 \$ "
+  PS1="$PS1 \n\$ "
 }
 
 #Jesper's old prompt:
