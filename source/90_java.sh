f99dbee7ba0977d69f4bebea05cfe44452c1131a
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-01-12 13:51:59 +0100

    chore: delete java custom file. Sdkman is fine, these days

--- source/90_java.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=bash
-
-# my java setup where needed
-# now using sdkman instead
-
-#export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home
