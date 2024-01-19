24f5563409797988172254a24cdf402bc6ba7fda
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-01-19 11:38:27 +0100

    chore: remove tmp file a little harder

--- bin/clean_tmp_files.sh
+++ bin/clean_tmp_files.sh
@@ -12,5 +12,5 @@ rm -rf ~/Library/Caches/Google/Chrome/Default/Media\ Cache/
 rm -rf ~/Library/Application\ Support/Atom/Cache/
 
 find ~/src -d 3 -type d -name "node_modules" -exec rm -rf '{}' +
-find ~/src/karnov -d 2 -type d -name "log" -exec rm -rf '{}' +
-
+find ~/src -d 4 -type d -name "target" -exec rm -rf '{}' +
+find ~/src -d 3 -type d -name "log" -exec rm -rf '{}' +
