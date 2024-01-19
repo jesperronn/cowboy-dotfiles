ef5a3f26a2daa570045571fd6e0ae4be9a52ae32
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-01-19 11:39:43 +0100

    chore STIL_HOME env variable

--- source/90_stil_alias.sh
+++ source/90_stil_alias.sh
@@ -1,9 +1,10 @@
 # shellcheck shell=bash
 
+export STIL_HOME="${HOME}/src/stil"
+
 # from https://www.keycloak.org/docs/latest/server_admin/index.html#installing-the-admin-cli
 export KEYCLOAK_HOME="${HOME}/src/stil/binaries/rh-sso-7.6"
 export PATH="${PATH}:${KEYCLOAK_HOME}/bin"
-
 export PATH="${PATH}:~/src/stil/support-tools/bin"
 
 stil_cert() {
