70287117007ab9b001a355a66501d2a72c09c7c6
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-03-16 00:56:35 +0100

    init ruby, but dont install stable since its old

--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 # Initialize my ruby via rvm
 
 setopt interactivecomments
 
 # install rvm and compile latest ruby
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-if [[ -s ~/.rvm/scripts/rvm ]] ; then source ~/.rvm/scripts/rvm ; fi
+\curl -sSL https://get.rvm.io | bash -s stable
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then source "$HOME/.rvm/scripts/rvm" ; fi
 rvm get head
 rvm reload
 
