b196f21ad8acf045327078e21e7512ff3906bf71
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-03-16 02:37:01 +0100

    prompt replaced by starship prompt (pure)
    Signed-off-by: Jesper Rønn-Jensen <jesperrr@gmail.com>

--- source/60_completion.sh
+++ source/60_completion.sh
@@ -1,9 +1,10 @@
 # shellcheck shell=bash
 
 # SSH auto-completion based on entries in known_hosts.
-if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
-fi
+# will not work on hashed host names
+# if [[ -e ~/.ssh/known_hosts ]]; then
+#   complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
+# fi
 
 
 # ssh completion (slow)
