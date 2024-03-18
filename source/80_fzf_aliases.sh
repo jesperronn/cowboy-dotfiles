200362f67637ca0da37f0a8a391499acbfc52a1d
Jesper Rønn-Jensen <jesperrr@gmail.com>
2024-03-18 11:15:27 +0100

    fzf for git, fzf eval for enabling key bindings

--- source/80_fzf_aliases.sh
+++ source/80_fzf_aliases.sh
@@ -1,35 +1,45 @@
 # shellcheck shell=bash
 
-# inspired by https://polothy.github.io/post/2019-08-19-fzf-git-checkout/
-fzf-git-branch() {
-    git rev-parse HEAD > /dev/null 2>&1 || return
+eval "$(fzf --bash)"
 
-    git branch --color=always --all --sort=-committerdate |
-        grep -v HEAD |
-        fzf --height 50% --ansi --no-multi --preview-window right:50% \
-            --preview 'git log -n 25 --color=always --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed "s/.* //" <<< {})' |
-        sed "s/.* //"
-}
+# git integration: https://github.com/junegunn/fzf-git.sh/blob/main/README.md
+# List of bindings
+#
+# CTRL-G CTRL-F for Files
+# CTRL-G CTRL-B for Branches
+# CTRL-G CTRL-T for Tags
+# CTRL-G CTRL-R for Remotes
+# CTRL-G CTRL-H for commit Hashes
+# CTRL-G CTRL-S for Stashes
+# CTRL-G CTRL-L for reflogs
+# CTRL-G CTRL-W for Worktrees
+# CTRL-G CTRL-E for Each ref (git for-each-ref)
+# ⚠️ You may have issues with these bindings in the following cases:
+#
+# CTRL-G CTRL-B will not work if CTRL-B is used as the tmux prefix
+# CTRL-G CTRL-S will not work if flow control is enabled, CTRL-S will freeze the terminal instead
+# (stty -ixon will disable it)
+# To workaround the problems, you can use CTRL-G {key} instead of CTRL-G CTRL-{KEY}.
+#
+# ⚠️ If zsh's KEYTIMEOUT is too small (e.g. 1), you may not be able to hit two keys in time.
+#
+# Inside fzf
+#
+# TAB or SHIFT-TAB to select multiple objects
+# CTRL-/ to change preview window layout
+# CTRL-O to open the object in the web browser (in GitHub URL scheme)
+#
+SRC=https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh
+DST=$HOME/bin/.fzf-git.sh
+if [[ ! -f "${DST}" ]];
+then
+  mkdir -p $HOME/bin/
+  curl -s "$SRC" -o "${DST}"
+  chmod +x "${DST}"
+fi
+
+[[ -f "${DST}" ]] && source "${DST}"
+
+unset SRC DST
 
 
-fzf-git-checkout() {
-    git rev-parse HEAD > /dev/null 2>&1 || return
-
-    local branch
-
-    branch=$(fzf-git-branch)
-    if [[ "$branch" = "" ]]; then
-        echo "No branch selected."
-        return
-    fi
-
-    # If branch name starts with 'remotes/' then it is a remote branch. By
-    # using --track and a remote branch name, it is the same as:
-    # git checkout -b branchName --track origin/branchName
-    if [[ "$branch" = 'remotes/origin/'* ]]; then
-        branch="${branch/remotes\/origin\//}"
-    fi
-        git checkout $branch;
-}
-
-alias gco="fzf-git-checkout"
